9c7af4a2885400e5f5be3a36c461d787c985b45e
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-11-20 23:05:05 +0300

    Removed other weeks

--- w3/roguelike.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <flecs.h>
-
-void init_roguelike(flecs::world &ecs);
-void process_turn(flecs::world &ecs);
-void print_stats(flecs::world &ecs);
--- w7/shootEmUp.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <flecs.h>
-
-void init_shoot_em_up(flecs::world &ecs);
-void process_game(flecs::world &ecs);
-void init_dungeon(flecs::world &ecs, char *tiles, size_t w, size_t h);
-
